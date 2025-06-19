53d3c52453afda2f3e449746fb8df9028554f916
Emily Sherer <elsherer1217@gmail.com>
2025-06-19 17:14:53 -0400

    updated project

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__name">Developed by Name Surname</p>
+      <p className="footer__date">2022</p>
+    </footer>
+  );
+}
+
+export default Footer;
